f5085a32ecc9f4ab326a10cf962c152817be2319
Marc Wiest <marc.wiest@gmail.com>
2017-09-18 20:26:38 +0200

    Replaced markup with action hook

--- index.php
+++ index.php
@@ -2,18 +2,6 @@
 
 get_header();
 
-while( have_posts() ) {
-    the_post();
-    echo '<article class="'.join( ' ', get_post_class() ).'">';
-    echo '<h2>'.get_the_title().'</h2>';
-    echo '<div>'.get_the_content().'</div>';
-    echo '</article>';
-}
-
-if ( is_active_sidebar( 'primary' ) ) {
-	echo '<div>';
-	dynamic_sidebar( 'primary' );
-	echo '</div>';
-}
+do_action( 'primera/index' );
 
 get_footer();
