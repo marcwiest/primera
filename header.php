4585ee5034b1ede1fc31c83719e49d2132dbf380
Marc Wiest <marc.wiest@gmail.com>
2017-10-03 21:11:33 +0200

    Removed meta tags and added div's

--- header.php
+++ header.php
@@ -2,11 +2,12 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php
-do_action( 'primera/header' );
+
+<div id="app">
+
+<div class="primera-site">
+<div class="primera-site-inner">
