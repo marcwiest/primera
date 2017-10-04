a3f3d96bf417cb4702136f4f577f6c5e2c49dd1b
Marc Wiest <marc.wiest@gmail.com>
2017-10-04 12:55:36 +0200

    Moved away from soley useing hooks for templating

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
 
 get_header();
+get_header( 'primary' );
 
-do_action( 'primera/index' );
+?>
 
+<div class="primera-main">
+<div class="container">
+    <div class="row">
+
+        <div class="col col-9">
+
+            <?php
+                if ( is_home() || is_post_type_archive() || is_archive() || is_search() ) {
+
+                    get_template_part( 'content', 'archive' );
+                }
+                else {
+
+                    get_template_part( 'content', 'singular' );
+                }
+            ?>
+
+        </div>
+
+        <div class="col col-3">
+
+            <?php dynamic_sidebar( 'primera_content_sidebar' ); ?>
+
+        </div>
+
+    </div>
+</div>
+</div>
+
+<?php
+
+get_footer( 'primary' );
 get_footer();
