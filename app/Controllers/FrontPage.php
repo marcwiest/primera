46b1a240df91dfb99297063673cfb4685ddaa826
Marc Wiest <marc.wiest@gmail.com>
2019-05-25 07:47:30 -0500

    Added example code

--- app/Controllers/FrontPage.php
+++ app/Controllers/FrontPage.php
@@ -11,6 +11,18 @@ defined('ABSPATH') || exit;
 
 class FrontPage extends Controller
 {
+    // Runs after this->data is set up, but before the class methods are run.
+    public function __before()
+    {
+        wp_enqueue_style(
+            'font-page',
+            get_theme_file_uri('public/css/page.css')
+        );
+    }
+
+    // Runs after all the class methods have run.
+    // public function __after() {}
+
     public function test()
     {
         return "FRONT PAGE DATA TEST";
