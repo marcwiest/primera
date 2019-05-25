860e45e197d7bef326bf63e09f1ccaa6678a93ec
Marc Wiest <marc.wiest@gmail.com>
2019-05-25 07:46:34 -0500

    Added tailwindcss config file

---
+++ source/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwindcss.com/docs/configuration
+
+module.exports = {
+    theme: {
+        screens: {
+            sm: '600px',
+            md: '900px',
+            lg: '1200px',
+        },
+    },
+};
