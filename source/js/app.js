05717ce9f54bc8c6e6eace5121de66ceabb243cb
Marc Wiest <marc.wiest@gmail.com>
2019-05-31 10:48:56 -0500

    Corrected import name

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,9 @@
-/** Primera theme */
+/*!* Primera theme */
 
 // import './vendor/fitvids';
 // import _ from './_lodash';
 import { $, $win, $doc, wp, vh, vw, fromTop } from './_globals';
-import tw from '../../tailwind.config.js';
+import twConfig from '../../tailwind.config.js';
 // import util from './utils';
 
 const app = {
