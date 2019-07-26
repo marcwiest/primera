562cc26fa25cf9a382d06f757e05d5a1d620e396
Marc Wiest <marc.wiest@gmail.com>
2019-07-26 14:01:40 -0700

    Added example templates

---
+++ app/Controllers/TmplDisplayPdf.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+// Exit if accessed directly.
+defined('WPINC') || exit;
+
+class TmplDisplayPdf extends Controller
+{
+    // Runs after this->data is set up, but before the class methods are run.
+    public function __before()
+    {
+        wp_enqueue_style(
+            'font-page',
+            get_theme_file_uri('public/css/page.css')
+        );
+    }
+
+    // Runs after all the class methods have run.
+    // public function __after() {}
+
+    public function test()
+    {
+        return "FRONT PAGE DATA TEST";
+    }
+}
