f2909d979cf955cfcceab4fbbc66c5b8b0bef732
Marc Wiest <marc.wiest@gmail.com>
2019-07-31 16:33:15 -0700

    Added example component

---
+++ source/views/components/example.blade.php
@@ -0,0 +1,5 @@
+{{--
+Example component.
+See: `app/Classes/BladeOne.php` for implementation.
+Use: `{{$slot}}` for component content/children and `@slot("name","value")` to add component specific vars.
+--}}
