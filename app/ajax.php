cdfe7520d5b99640cbece09246fb68832955b041
Marc Wiest <marc.wiest@gmail.com>
2019-10-06 22:17:09 +0200

    Added usage example

---
+++ app/ajax.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+defined('WPINC') || exit;
+
+// Usage example:
+// add_action('do-something', ['\App\Controllers\App', 'do_something']);
+// add_action('do-something', [
+//     'nopriv' => flase,
+//     'priv' => ['\App\Controllers\App', 'do_something'],
+// ]);
