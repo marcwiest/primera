9eb8148be31c519bb012789cf8055c75101171cb
Marc Wiest <marc.wiest@gmail.com>
2019-10-06 22:18:23 +0200

    Added example code

---
+++ app/rest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+defined('WPINC') || exit;
+
+add_action('rest_api_init', function() {
+
+    register_rest_route( 'primera/v1', '/get-pdf-from-post/', [
+        'methods'  => 'POST',
+        'callback' => '\App\Controllers\App::get_pdf_from_post',
+    ], true );
+});
