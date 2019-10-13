e99586366a7e50d8bf31193f662d88d93e18ff5c
Marc Wiest <marc.wiest@gmail.com>
2019-10-13 10:21:08 +0200

    Remove missplaced rest route

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -18,12 +18,12 @@ class App extends Controller
 
     public function site_name(): string
     {
-        return get_bloginfo( 'name', 'display' );
+        return get_bloginfo('name', 'display');
     }
 
     public function site_desc(): string
     {
-        return get_bloginfo( 'description', 'display' );
+        return get_bloginfo('description', 'display');
     }
 
     public function logo_url(): string
@@ -46,34 +46,5 @@ class App extends Controller
             'echo'           => false,
             'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
         ]) ?: '';
-    }
-
-    /**
-    * REST get PDF from post.
-    * REST routes must be defined statically.
-    */
-    public static function get_pdf_from_post(\WP_REST_Request $req)
-    {
-        $resp = [
-            'success' => true,
-            'post' => null,
-            'file' => null,
-            'msg' => [
-                'unsuccessful' =>  __("We are sorry, but it seems something went wrong. Please try again or contact us for help.", 'primeraTextDomain'),
-                'no_files' => __("We are sorry, but it seems we couldn't find what you are looking for. Please check your form settings or contact us for help.", 'primeraTextDomain'),
-            ],
-        ];
-
-        if ( empty($req['post-id']) ) {
-            $resp['success'] = false;
-            return \rest_ensure_response($resp);
-        }
-
-        $resp['post'] = get_post($req['post-id']);
-
-        $file_id      = get_post_field('attched_pdf', $resp['post'], 'raw');
-        $resp['file'] = $file_id ? get_post($file_id) : false;
-
-        return \rest_ensure_response($resp);
     }
 }
