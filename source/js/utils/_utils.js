c8467d9aad420a34c4fc64d0df1e99d2993a6576
Marc Wiest <marc.wiest@gmail.com>
2019-10-25 06:54:23 +0200

    Moved utils

---
+++ source/js/utils/_utils.js
@@ -0,0 +1,173 @@
+
+// export default 'test';
+const $  = window.jQuery || {};
+const wp = window.wp || {};
+const localized = window.primeraFunctionPrefixLocalizedData || {};
+const localizedData = localized;
+
+// TODO: Fn is unnecessary since PostCSS offers a fallback: https://github.com/postcss/postcss-custom-properties
+// TODO: https://developer.mozilla.org/en-US/docs/Web/API/CSS/supports
+// gist.github.com/wesbos/8b9a22adc1f60336a699
+let supportsCssCustomProps = (() => {
+
+    var color = 'rgb(255, 198, 0)';
+    var el = document.createElement('span');
+
+    el.style.setProperty('--color', color);
+    el.style.setProperty('background', 'var(--color)');
+    document.body.appendChild(el);
+
+    var styles = getComputedStyle(el);
+    var doesSupport = styles.backgroundColor === color;
+    document.body.removeChild(el);
+
+    return doesSupport;
+})();
+
+let wpAjaxPost = (action, data, settings) => {
+
+    if (! $.isPlainObject(data)) {
+        console.error('The "data" parameter must be a plain obejct!');
+        return {};
+    }
+
+    settings = settings || {};
+    settings.type = 'POST';
+    settings.url = localized.ajaxUrl;
+    settings.data = $.extend( data || {}, { action: action, nonce: localized.ajaxNonce });
+
+    return $.ajax(settings);
+};
+
+export { supportsCssCustomProps, wpAjaxPost };
+
+export default {
+
+    rest : {
+
+        /**
+        * Sends a POST request to a REST endpoint.
+        *
+        * @since  1.0
+        * @param  {(string|object)}  action  The route to send the request to (e.g. 'do-stuff').
+        * @param  {object}  data  Optional. The data to populate $_POST with.
+        */
+        post : function( route, data ) {
+
+            var options;
+
+            if ( _.isObject(route) ) {
+                options = route;
+            }
+            else {
+                options = {
+                    data : data || {}
+                };
+            }
+
+            options = _.defaults( options, {
+                type       : 'POST',
+                url        : localizedData.restUrl + route,
+                beforeSend : function( xhr ) {
+                    xhr.setRequestHeader( 'X-WP-Nonce', localizedData.restNonce );
+                }
+            });
+
+            return $.ajax( options );
+        }
+
+    },
+
+    isAndroid : function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+
+    isBlackBerry : function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+
+    isIOS : function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+
+    isOperaMini : function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+
+    isIEMobile : function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+
+    isMobile : function() {
+        return (
+            this.isAndroid() ||
+            this.isIOS() ||
+            this.isOperaMini() ||
+            this.isBlackBerry() ||
+            this.isIEMobile()
+        );
+    },
+
+    /**
+    * Check if browser is MS IE.
+    */
+    isIE : function() {
+
+        var ua   = window.navigator.userAgent,
+            msie = ua.indexOf("MSIE ");
+
+        if ( msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) ) {
+            return true;
+        }
+        return false;
+    },
+
+    /**
+    * Check if browser is MS Edge.
+    */
+    isEdge : function() {
+
+        if ( document.documentMode || /Edge/.test(navigator.userAgent) ) {
+            return true;
+        }
+        return false;
+    },
+
+    /**
+    * Gets the viewport width, excluding the scrollbars.
+    *
+    * @since  1.0
+    */
+    getViewportWidth : function() {
+        return document.body.clientWidth;
+    },
+
+    /**
+    * Gets the viewport height, excluding the scrollbars.
+    *
+    * @since  1.0
+    */
+    getViewportHeight : function() {
+        return document.body.clientHeight;
+    },
+
+    /**
+    * Gets the distance of an element to the viewport's edge.
+    *
+    * @param  jQuery  jQuery element of which to get the viewport offset.
+    * @since  1.0
+    */
+    getViewportOffset : function ( $elem ) {
+
+        var $window    = jQuery(window),
+            scrollLeft = $window.scrollLeft(),
+            scrollTop  = $window.scrollTop(),
+            offset     = $elem.offset();
+
+        return {
+            left : offset.left - scrollLeft,
+            top  : offset.top - scrollTop
+        };
+    }
+
+};
