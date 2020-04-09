8233e330278b64629a4bb5f603928b5e30cf1cf2
Marc Wiest <marc.wiest@gmail.com>
2020-04-09 16:06:59 +0200

    Add New Fn

--- app/backcompat.php
+++ app/backcompat.php
@@ -3,9 +3,15 @@
 defined('ABSPATH') || exit;
 
 if (! function_exists('wp_body_open')) :
-// Backward compatibility.
 function wp_body_open()
 {
-    do_action( 'wp_body_open' );
+    do_action('wp_body_open');
+}
+endif;
+
+if (! function_exists('apply_shortcodes')) :
+function apply_shortcodes($string)
+{
+    do_shortcode($string);
 }
 endif;
