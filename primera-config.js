fe408caa0d4989514080f53d610c3f6d6bec2ada
Marc Wiest <marc.wiest@gmail.com>
2020-04-10 22:20:23 +0200

    Add Props

--- primera-config.js
+++ primera-config.js
@@ -1,21 +1,60 @@
+pkg = require('./package.json')
 
 module.exports = {
 
+    name: pkg.name || 'primera',
+
     /**
-    * The files to be included when running `$ npm run make-tag`.
+    * The theme's text domain as specified in `style.css`.
     */
-    tagFiles: [
-        'app',
-        'languages',
-        'public',
-        'source/views',
-        'templates',
+    textDomain: 'primeraTextDomain',
+
+    /**
+    * The current version of the project.
+    *
+    * This should match versions inside `CHANGELOG.md`, `package.json`, and `style.css`.
+    */
+    version: pkg.version || '1.0.0',
+
+    /**
+    * The files to be included for CLI command `$ npm run build`.
+    */
+    buildFiles: [
         'vendor',
-        'CHANGELOG.md',
-        'LICENSE.md',
+        'app',
+        'source/views',
+        'public',
+        'templates',
+        'languages',
+        '.htaccess',
+        'functions.php',
         'index.php',
         'index.php',
         'screenshot.png',
         'style.css',
+        'LICENSE.md',
+        'README.md',
+        'CHANGELOG.md',
     ],
+
+    /**
+    * The files which will be scanned for translatable strings.
+    *
+    * See node-glob for instructions (https://github.com/isaacs/node-glob#glob-primer).
+    */
+    potFiles: [
+        '**/*.php',
+        '!vendor/**/*.php',
+        '!dist/**/*.php',
+    ],
+
+    /**
+    * The place where the zipped archives live.
+    */
+    archiveDirPath: 'dist/archive',
+
+    /**
+    * Where to place the build.
+    */
+    buildDirPath: `dist/${pkg.name || 'primera'}`,
 }
