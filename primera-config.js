df3cc2d070559a85df4ae2f9ab95b51a243eeb38
Marc Wiest <marc.wiest@gmail.com>
2020-04-10 11:37:58 +0200

    Add Node Config

---
+++ primera-config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+
+    /**
+    * The files to be included when running `$ npm run make-tag`.
+    */
+    tagFiles: [
+        'app',
+        'languages',
+        'public',
+        'source/views',
+        'templates',
+        'vendor',
+        'CHANGELOG.md',
+        'LICENSE.md',
+        'index.php',
+        'index.php',
+        'screenshot.png',
+        'style.css',
+    ],
+}
