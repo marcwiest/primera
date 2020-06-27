eb88ec50f504370cae2e1ef212cb017333623565
Marc Wiest <marc.wiest@gmail.com>
2020-06-27 08:32:44 +0200

    Code cleanup

--- functions.php
+++ functions.php
@@ -14,6 +14,17 @@ $primeraError = function( $message, $subtitle='', $title='' ) {
 };
 
 /**
+* Ensure composer dependencies are loaded.
+*/
+if ( ! file_exists( $composer = get_parent_theme_file_path( 'vendor/autoload.php' ) ) ) {
+    $primeraError(
+        __( 'You must run <code>composer install</code> from the theme directory.', 'primeraTextDomain' ),
+        __( 'Autoloader not found', 'primeraTextDomain' )
+    );
+}
+require_once $composer;
+
+/**
 * Ensure app files are loaded.
 *
 * Add or remove files to the array as needed, locate_template supports child theme overrides.
@@ -25,17 +36,6 @@ foreach ([
 ] as $file) {
     locate_template("app/{$file}.php", true, true);
 }
-
-/**
-* Ensure composer dependencies are loaded.
-*/
-if ( ! file_exists( $composer = get_parent_theme_file_path( 'vendor/autoload.php' ) ) ) {
-    $primeraError(
-        __( 'You must run <code>composer install</code> from the theme directory.', 'primeraTextDomain' ),
-        __( 'Autoloader not found', 'primeraTextDomain' )
-    );
-}
-require_once $composer;
 
 /**
 * Ensure config files are loaded.
